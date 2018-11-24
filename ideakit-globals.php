1dbf95b71f390c51ceeb7485c34bde01696ef85b
Bryant <bryant.cole@nerdintel.com>
2018-11-24 14:16:50 -0500

    Update ideakit-globals.php

--- ideakit-globals.php
+++ ideakit-globals.php
@@ -1,26 +1,27 @@
 <?php
 /**
  * @package IdeaKit_Globals
- * @version 0.2
+ * @version 0.2.1
  */
 /*
 Plugin Name: IdeaKit Globals
 Plugin URI: https://nerdintel.com/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Nerd Inelligence Agency
-Version: 0.2
+Version: 0.2.1
 Author URI: https://getideakit.com/
 */
 
 if (!defined('ABSPATH'))
     exit; // Exit if accessed directly
-    //Plugin update check
-    require 'plugin-update-checker/plugin-update-checker.php';
-    $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-    	'https://github.com/Nerd-Intelligence-Agency/ideakit-globals/',
-    	__FILE__,
-    	'ideakit-globals'
-    	);
+
+//Plugin update check
+require plugin_dir_path( __FILE__ ).'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+  'https://github.com/Nerd-Intelligence-Agency/ideakit-globals/',
+  __FILE__,
+  'ideakit-globals'
+);
 
 //set awesome support to saas mode on
 define('WPAS_REMOTE_NOTIFICATIONS_OFF', true) ;
