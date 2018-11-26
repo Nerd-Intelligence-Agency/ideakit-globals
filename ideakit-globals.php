0cf227eedc39c5dbc3edcf7cab7fb272de9b10b6
Bryant Cole <bryant.cole@nerdintel.com>
2018-11-26 17:53:48 -0500

    Update ideakit-globals.php

--- ideakit-globals.php
+++ ideakit-globals.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * @package IdeaKit_Globals
- * @version 0.2.2
+ * @version 0.2.3
  */
 /*
 Plugin Name: IdeaKit Globals
 Plugin URI: https://nerdintel.com/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Nerd Inelligence Agency
-Version: 0.2.2
+Version: 0.2.3
 Author URI: https://getideakit.com/
 */
 
